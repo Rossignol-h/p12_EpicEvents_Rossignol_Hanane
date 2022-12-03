f35c5f3e79caa6dd662c8178562504278aba0442
Rossignol-h <ha.rossignol@laposte.net>
2022-12-03 19:29:09 +0100

    feat(authentication): add custom form to useradmin

--- backend/authentication/admin.py
+++ backend/authentication/admin.py
@@ -1,11 +1,52 @@
 from django.contrib.auth.admin import UserAdmin
 from django.contrib import admin
+from django import forms
 
 from .models import Employee
 
+# ======================================================== CUSTOM MODEL FORM
 
-class CustomUserAdmin(UserAdmin):
 
+class EmployeeAdminForm(forms.ModelForm):
+    """
+        Form for creating new employees. Includes all the required
+        fields, & repeated password.
+    """
+    password1 = forms.CharField(label='Password', widget=forms.PasswordInput)
+    password2 = forms.CharField(label='Password confirmation', widget=forms.PasswordInput)
+    
+    class Meta:
+        model = Employee
+        fields = '__all__'
+
+    def clean_password2(self):
+        # Check that the two password entries match
+        password1 = self.cleaned_data.get("password1")
+        password2 = self.cleaned_data.get("password2")
+        if password1 and password2 and password1 != password2:
+            raise forms.ValidationError("Passwords don't match")
+        return password2
+
+    def clean_email(self):
+        email = self.cleaned_data["email"]
+        if not email.endswith('@epicevents.com'):
+            raise forms.ValidationError("No Vampires")
+        return self.cleaned_data["email"]
+
+    def save(self, commit=True):
+        # Save the provided password in hashed format
+        user = super(EmployeeAdminForm, self).save(commit=False)
+        user.set_password(self.cleaned_data["password1"])
+        if commit:
+            user.save()
+        return user
+
+# ======================================================== CUSTOM USER ADMIN
+
+
+class EmployeeAdmin(UserAdmin):
+
+    add_form = EmployeeAdminForm
     search_fields = ('email',)
     list_display = ['email', 'role', 'phone_number', 'is_superuser']
     list_filter = ['role',]
@@ -13,7 +54,7 @@ class CustomUserAdmin(UserAdmin):
     add_fieldsets = (
         (None, {
             'classes': ('wide',),
-            'fields': ('email', 'password1','password2' ),
+            'fields': ('email', 'password1', 'password2','phone_number' , 'role' ),
         }),
     )
 
@@ -25,6 +66,6 @@ class CustomUserAdmin(UserAdmin):
     )
 
     radio_fields = {'role': admin.HORIZONTAL}
-    ordering = ('email',)
+    ordering = ('id',)
 
-admin.site.register(Employee, CustomUserAdmin)
+admin.site.register(Employee, EmployeeAdmin)
--- backend/authentication/serializers.py
+++ backend/authentication/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework.serializers import ModelSerializer
+from rest_framework import serializers
 from .models import Employee
 
 
@@ -7,6 +8,14 @@ class EmployeeSerializer(ModelSerializer):
         model = Employee
         fields = ['id', 'email','password', 'phone_number','role','groups', 'is_staff', 'is_superuser']
         read_only_fields = ['is_staff', 'is_superuser', 'groups']
+
+    def validate(self, data):
+        """
+            Check that email ends with @epicevent.com.
+        """
+        if not data['email'].endswith('@epicevents.com'):
+            raise serializers.ValidationError("Wrong email format: Please make sure to write @epicevent.com")
+        return data
 
     def create(self, validated_data):
         return Employee.objects.create_user(**validated_data)
