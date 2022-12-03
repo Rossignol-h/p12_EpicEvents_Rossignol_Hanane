6239b2af485fcbb3fb794ad16d62313ba96249a0
Rossignol-h <ha.rossignol@laposte.net>
2022-12-03 19:22:18 +0100

    feat(authentication): add custom useradmin

--- backend/authentication/admin.py
+++ backend/authentication/admin.py
@@ -1,3 +1,30 @@
+from django.contrib.auth.admin import UserAdmin
 from django.contrib import admin
 
-# Register your models here.
+from .models import Employee
+
+
+class CustomUserAdmin(UserAdmin):
+
+    search_fields = ('email',)
+    list_display = ['email', 'role', 'phone_number', 'is_superuser']
+    list_filter = ['role',]
+
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1','password2' ),
+        }),
+    )
+
+    fieldsets = (
+        (None, {'fields': ('email',)}),
+        ('Contact', {'fields': ('phone_number',)}),
+        ('Security', {'fields': ('password',)}),
+        ('Permissions', {'fields': ('role',)}),
+    )
+
+    radio_fields = {'role': admin.HORIZONTAL}
+    ordering = ('email',)
+
+admin.site.register(Employee, CustomUserAdmin)
