f6186b1d81110c974b5e5986aa52852144b4f48a
Rossignol-h <ha.rossignol@laposte.net>
2022-12-05 10:50:31 +0100

    feat(client): add custom client admin

--- backend/client/admin.py
+++ backend/client/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Client
 
-# Register your models here.
+
+class ClientAdmin(admin.ModelAdmin):
+    search_fields = ('company_name', 'email')
+    list_display = ['company_name', 'email', 'is_prospect','sales_contact_id']
+    list_filter = ['is_prospect']
+
+admin.site.register(Client, ClientAdmin)
