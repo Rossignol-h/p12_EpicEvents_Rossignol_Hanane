0bac561f9d3574de72f88864775b306f60dc24ae
Rossignol-h <ha.rossignol@laposte.net>
2022-12-06 23:37:47 +0100

    feat(event): add views to event

--- backend/event/views.py
+++ backend/event/views.py
@@ -1,3 +1,72 @@
-from django.shortcuts import render
+from rest_framework.permissions import DjangoModelPermissions
+from rest_framework.exceptions import ValidationError
+from rest_framework.response import Response
+from rest_framework import viewsets, status
+from rest_framework import filters
 
-# Create your views here.
+from django.core.exceptions import ObjectDoesNotExist
+from django.shortcuts import get_object_or_404
+from django.http import Http404
+
+from .serializers import EventSerializer
+from permissions import EventPermission
+from .models import Event
+from contract.models import ContractStatus
+
+# =========================================================== EVENT VIEW
+
+
+class EventsViewSet(viewsets.ReadOnlyModelViewSet):
+    """
+    List all events.
+    """
+    serializer_class = EventSerializer
+    queryset = Event.objects.all()
+    permission_classes = [DjangoModelPermissions]
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['event_date', 'client_id__email', 'client_id__company_name']
+
+
+class EventViewSet(viewsets.ModelViewSet):
+    """
+    Add, retrieve, update and delete an event to the crm.
+    """
+
+    serializer_class = EventSerializer
+    queryset = Event.objects.all()
+    permission_classes = [DjangoModelPermissions, EventPermission]
+    search_fields = ['event_date', 'client_id', 'client_id.email']
+    filter_backends = (filters.SearchFilter,)
+
+    def get_contract_status(self):
+        lookup_url_kwarg = self.kwargs['contract_id']
+        return get_object_or_404(ContractStatus, pk=lookup_url_kwarg)
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        try:
+            current_contract = get_object_or_404(ContractStatus, pk=self.kwargs['contract_id'])
+            serializer.is_valid(raise_exception=True)
+            new_event = serializer.save(
+                client_id=current_contract.contract.client_id, 
+                event_status=current_contract)
+
+            return Response({
+                'New event': EventSerializer(new_event, context=self.get_serializer_context()).data,
+                'message': f"this event is successfully added to the crm"},
+                status=status.HTTP_201_CREATED)
+
+        except ObjectDoesNotExist:
+            raise Http404
+
+
+    def destroy(self, request, *args, **kwargs):
+        try:
+            event_to_delete = Event.objects.get(id=self.kwargs['pk'])
+            self.perform_destroy(event_to_delete)
+            return Response(
+                        {'message': "This event is successfully deleted"},
+                        status=status.HTTP_204_NO_CONTENT)
+
+        except ObjectDoesNotExist:
+            raise ValidationError("This event doesn't exist")
